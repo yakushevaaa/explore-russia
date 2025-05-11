8f3f5abc6287d4a4a8514fd56b3a74e6bcb4f933
yakushevaaa <muzisnvlada@gmail.com>
2025-05-11 23:37:19 +0300

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import "@/styles/fonts.scss";
+import "@/styles/reset.scss";
+import "@/styles/variables.scss";
+
+function App() {
+  //тут должен быть layout
+
+  return <h1>app</h1>;
+}
+
+export default App;
